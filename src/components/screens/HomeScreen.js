37f191cb9bbfba5d4ac1b6949af63e284f1e8d6a
vander makadi <vanotis720@gmail.com>
2024-02-07 14:59:15 +0200

    home animation

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import { Image, StatusBar, Text, TouchableOpacity, View } from "react-native";
+import LottieView from "lottie-react-native";
 
 export default function HomeScreen({ navigation }) {
 
@@ -7,21 +8,29 @@ export default function HomeScreen({ navigation }) {
     }
 
     return (
-        <View className="flex-1 bg-white justify-center items-center">
+        <View className="flex-1 bg-[#CECED0] p-5">
             <StatusBar style="auto" />
+            <View className="h-2/3">
+                <LottieView
+                    source={require('../../../assets/animations/Animation-1707310095949-stepup-stairs.json')}
+                    autoPlay
+                    loop
+                    speed={0.5}
+                    style={{ flex: 1 }}
+                />
+            </View>
+            <View className="h-1/3">
+                <Text className="font-semibold text-lg text-center">Élargis tes  connaissances en t’amusant !</Text>
 
-            {/* <Image source={require('../../assets/icon.png')} className="w-1/3 resize" /> */}
-            <Text className="text-4xl text-bold mt-5">QuizPop</Text>
-            <Text className="font-light text-lg text-center">Élargissez vos connaissances avec QuizPop !</Text>
-
-            <TouchableOpacity
-                onPress={handleStart}
-                className="absolute bg-[#004643] bottom-5 left-5 right-5 h-12 rounded-2xl"
-            >
-                <Text className="text-lg text-white m-auto font-bold">
-                    Commencer
-                </Text>
-            </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={handleStart}
+                    className="bg-[#004643] mt-5 py-3 px-5 rounded-2xl"
+                >
+                    <Text className="text-lg text-white m-auto font-bold">
+                        Allons-y
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
